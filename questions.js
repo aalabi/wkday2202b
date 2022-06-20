d10aed90fd84c4a9ff0445c8261e848510b1c438
Alabi A <alabi10@yahoo.com>
2022-06-20 12:59:04 +0100

    added question.js

---
+++ questions.js
@@ -0,0 +1,12 @@
+let questionAnswer = [
+  {
+    question: "What is the firstname of Alabi",
+    options: ["Alabi", "Adebayo", "Osagbokan", "Ivie"],
+    answer: 1,
+  },
+  {
+    question: "Who is the president of Nigeria",
+    options: ["Buhari", "Tinubu", "Alabi", "John Terry"],
+    answer: 2,
+  },
+];
