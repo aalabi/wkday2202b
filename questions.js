2d78ef19ced9703f3c660129ad63e2556d1ae20a
dmoses <ogundipeoluwadahunsi@gmail.com>
2022-06-20 13:19:48 -0700

    added question to question.js

--- questions.js
+++ questions.js
@@ -8,5 +8,10 @@ let questionAnswer = [
     question: "Who is the president of Nigeria",
     options: ["Buhari", "Tinubu", "Alabi", "John Terry"],
     answer: 2,
+  },  
+  {
+    question: "The best football club in world is",
+    options: ["Arsenal FC", "FC Barcelona", "Real Madrid", "Chelsea FC"],
+    answer: 1,
   },
 ];
